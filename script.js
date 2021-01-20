b702b9955c2713491e832cf8636158831ac1af7c
AIFighting <77727434+AIFighting@users.noreply.github.com>
2021-01-20 11:52:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var block2 = document.getElementById("block2");
+var villain = document.getElementById("villain");
+var weapon = document.getElementById("weapon");
+var counter=0;
+var countervil=0;
+function jump(){
+    if(comm == 1){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },900);
+}
+function jumpdown(){
+    if(comm == 1){return}
+    character.classList.add("animatedown");
+    setTimeout(function(){
+        character.classList.remove("animatedown");
+    },900);
+}
+function shoot(){
+    weapon.classList.add("animateshoot");
+    setTimeout(function(){
+        weapon.classList.remove("animateshoot");
+    },900);
+}
+function villainfunc(){
+    villain.classList.add("animatevillain");
+    setTimeout(function(){
+        villain.classList.remove("animatevillain");
+    },4000);
+}
+var checkMonster = setInterval(function() {
+    let villainright = parseInt(window.getComputedStyle(villain).getPropertyValue("right"));
+    let kannuright = parseInt(window.getComputedStyle(weapon).getPropertyValue("right"));
+    let characterright = parseInt(window.getComputedStyle(character).getPropertyValue("right"));
+    if((villainright != 600) && ((kannuright-villainright) <0) && (kannuright-villainright) >-80){
+        weapon.classList.remove("animateshoot");
+        villain.classList.remove("animatevillain");
+    }
+	  else if ((villainright != 600) && ((characterright-villainright)<20)) {
+		    block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 4s infinite linear";
+        block2.style.animation = "block2 4s 2s  infinite linear";
+	}
+}, 10);
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=168){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 4s infinite linear";
+        block2.style.animation = "block2 4s 2s infinite linear";
+    }else{
+        counter++;
+        //Values for boundaries can be adjusted to smoothen the gameplay
+        if(counter > 720 && counter < 2100){
+            block.style.animation = "block 3.5s infinite linear";
+            block2.style.animation = "block2 3.5s 1s infinite linear";
+        }
+        else if(counter > 2099 && counter < 3000){
+            block.style.animation = "block 3s infinite linear";
+            block2.style.animation = "block2 3s 1s infinite linear";
+        }
+        else if(counter > 2999 && counter < 3900){
+            block.style.animation = "block 2.5s infinite linear";
+            block2.style.animation = "block2 2.5s 1s infinite linear";
+        }
+        else if(counter > 3999 && counter < 4750){
+            block.style.animation = "block 2s infinite linear";
+            block2.style.animation = "block2 2s 0.5s infinite linear";
+        }
+        else if(counter > 4749 && counter < 6000){
+            block.style.animation = "block 1.5s infinite linear";
+            block2.style.animation = "block2 1.5s 0.5s infinite linear";
+        };
+
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        //Function for activating villain, change values for quantity
+        if((counter/100)%7==0 && (counter!= 0)){
+          villainfunc();
+        };
+    }
+}, 10);
